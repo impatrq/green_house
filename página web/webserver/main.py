85e937b2cefebb85fa3cf61879e31267a498a4aa
JuanSaz <juan.snchez03@gmail.com>
2022-09-15 16:55:11 -0300

    refactor: corregir idioma
    Se emprolijó el codigo. Se pasó la mayoria del mismo a inglés (parte del codigo estaba en español e ingles). Se agregaron comentarios para mejorar la comprensión del código.

--- página web/webserver/main.py
+++ página web/webserver/main.py
@@ -1,3 +1,12 @@
+#                                           // GREENHOUSE //
+#
+#   El proyecto se basa en un sistema de control mediante sensores y actuadores. 
+#   Se utilizan: DHT22, LDRs, HL69.
+#   Protocolo Web: Socket
+
+
+
+
 from hashlib import sha1
 from machine import Pin, ADC, Timer, RTC
 from time import sleep
@@ -13,97 +22,97 @@ relayVentiladores = Pin(25, Pin.OUT)
 relayLuces = Pin(32, Pin.OUT)
 
 #Definicion variables
-humedadSuelo = ""
-ldrEstado = ""
+soilHumidity = ""
+ldrState = ""
 temp = ""
 hum = ""
 
-lock = _thread.allocate_lock()
-        
+lock = _thread.allocate_lock()                                  # El código tiene dos hilos, uno mide con los sensores, y el otro espera una conexion
+                                                                # de un socket. Ambos trabajan de manera independiente mediante locks (semaphores)
 def setNetwork():
     lock.acquire()
-    print("Inicializando WebServer. Esperando Conexion...")
-    conn, addr = s.accept()
+    print("Initializing WebServer. Awaiting Connection...")
+    conn, addr = s.accept()                                     # Espera una conexion mediante un socket
     print('Got a connection from %s' % str(addr))
     request = conn.recv(1024)
     request = str(request)   
-    update = request.find('/update')        
+    update = request.find('/update')                            # Busca /update en URL     
        
     if update == 6:
         print('update') 
         
-    response = webPage()
+    response = webPage()                                        # Abre el HTML en la pagina
     conn.send('HTTP/1.1 200 OK\n')
     conn.send('Content-Type: text/html\n')
     conn.send('Connection: close\n\n')
-    conn.sendall(response)
-    conn.close()
-    #tim0 =Timer(0)
-    #tim0.init(period=10000, mode=Timer.PERIODIC, callback=getEstados)         #Este timer se activa cada 1 hora (3600000 milisegundos)                                                            #de la humedad del suelo
-    lock.release()                                                             #para verificar el estado
+    conn.sendall(response)                                      # Envia el HTML
+    conn.close()                                                
+    lock.release()                                                             
 
-def getSensors():
-    lock.acquire()
-    print("Inicializando sensores...")
+def getSensors():                                            # getSensors es una funcion que engloba todas las funciones que toman resultados de mediciones
+    lock.acquire()                                           # Toma el lock para no sobre escribir/tomar mal datos
+    print("Initializing sensors...")
     getLDR()
     getHL()
     getDHT()
     lock.release()
 
 def getLDR():
-    global ldrEstado
-    ldrRead1 = adcLDR1.read() * (3.3/4096)
+    global ldrState
+    ldrRead1 = adcLDR1.read() * (3.3/4096)                                          # Conversion ADC // valor lectura
     RLDR1 = (ldrRead1 * 10000)/(3.3 - ldrRead1)
     print("El valor de la resistencia del LDR1 es: {:.0f}" .format(RLDR1))
     
-    ldrRead2 = adcLDR2.read() * (3.3/4096)         # 1000 ya está iluminado (full)
-    RLDR2 = (ldrRead2 * 10000)/(3.3 - ldrRead2)    # 40952430 está oscuro (full)
+    ldrRead2 = adcLDR2.read() * (3.3/4096)                                           # 1000 ya está iluminado (full)
+    RLDR2 = (ldrRead2 * 10000)/(3.3 - ldrRead2)                                      # 40952430 está oscuro (full)
     print("El valor de la resistencia del LDR2 es: {:.0f}" .format(RLDR2))
 
-    ldrPromedio = (RLDR1 + RLDR2) / 2
-    print("El promedio es: ", ldrPromedio)
-    if(ldrPromedio > 2000):
-        ldrEstado = " Oscuro"
+    ldrAverage = (RLDR1 + RLDR2) / 2                                                 # Calculo promedio de iluminacion entre ambos LDR // WIP
+    print("Valor promedio LDRs: ", ldrAverage)
+    if(ldrAverage > 2000):
+        ldrState = "Oscuro"
     else:
-        ldrEstado = " Iluminado"
+        ldrState = "Iluminado"
     sleep(1)
 
 def getHL():
-    adcHL.atten(ADC.ATTN_11DB)
-    HLRead= adcHL.read()
+    adcHL.atten(ADC.ATTN_11DB)                                                        # La atenuacion tuvo que ser cambiada para poder tener mejores
+    HLRead= adcHL.read()                                                              # Valores de lectura con el ADC.
     sleep(1)
-    global humedadSuelo
-    humedadSuelo = str(HLRead)
-    print("La humedad de suelo: " ,humedadSuelo)
+    global soilHumidity
+    soilHumidity = str(HLRead)
+    print("La humedad de suelo: " ,soilHumidity)
         
 def getDHT():
   global temp, hum
   temp = hum = 0
   try:
-    sensor.measure()
-    temp = sensor.temperature() 
-    hum = sensor.humidity()
-    temp = str(temp)
+    sensor.measure()                                                                  
+    temp = sensor.temperature()                                                       # Lectura de temperatura
+    hum = sensor.humidity()                                                           # Lectura de humedad ambiente
+    temp = str(temp)                                        
     hum = str(hum)
   except OSError as e:
     return('Failed to read sensor.')
 
-def getEstados(timer):
-    watering()
+def getStates(timer):                                                                 # Esta es la funcion que el timer activa cada vez que se triggerea 
+    #                                                                                   el mismo, se trata de una funcion que pregunta por el estado de los sensores    
+    #getSensors()                                                                     # Se tiene que llamar a esta funcion para tomar datos recientes de los sensores y no desactualizados
+    watering()                                                                        
     lighting()
     cooling()
     
 def lighting():
-    global ldrEstado
-    if(ldrEstado == false):
+    global ldrState
+    if(ldrState == "Oscuro"):
         print("Debo iluminar")
     else:
         print("No debo iluminar")
 
 def watering():
     optimalHumidity = 800
-    global humedadSuelo
-    if(humedadSuelo >= optimalHumidity):
+    global soilHumidity
+    if(soilHumidity >= optimalHumidity):
         print("Debo regar")
         getTime()
     else:
@@ -112,21 +121,21 @@ def watering():
 def cooling():
     optimalTemp = 20
     global temp
-    if(temp > optimalTemp):
+    if(float(temp) > optimalTemp):
         print("Hace calor")
-    elif(temp == optimalTemp):
+    elif(float(temp) == optimalTemp):
         print("No hacer nada")
     else:
         print("Hace frio")
         
 def getTime():
     rtc = RTC()
-    fecha = rtc.datetime()
-    fechaActual = fecha[4:6]
-    print(str(fechaActual))
+    date = rtc.datetime()
+    currentDate = date[4:6]
+    print(str(currentDate))
     
 def webPage():
-    global humedadSuelo, ldrEstado, temp, hum
+    global soilHumidity, ldrState, temp, hum
     html = f"""
             <!DOCTYPE html>
             <html lang="es">
@@ -158,19 +167,19 @@ def webPage():
                         background-image: url("https://i.ibb.co/C8Q6ddJ/Background-Guias.png");
                         display: flex;
                         flex-direction: column;
-                        justify-content: center;
-                        align-items: center;
                     }}
 
                     .contenedor {{
+                        margin: 0 auto;
                         display: flex;
                         flex-direction: column;
                         align-items: center;
                         border-radius: 5rem;
                         max-width: fit-content;
+                        padding: 0 2rem;
                         height: fit-content;
-                        padding: 0 5% 0 5%;
                         background-color: rgba(0, 0, 0, 0.2);
+                        box-sizing: border-box;
                     }}
 
                     .contenedor h2 {{
@@ -178,12 +187,18 @@ def webPage():
                         text-align: center;
                         font-size: 2.5rem;
                     }}
+
                     .contenedor-variables {{
-                        display: grid;
+                        /*display: grid;
                         grid-template-columns: repeat(2, 1fr);
                         grid-gap: 0 1rem;
                         font-size: 1.5rem;
+                        color: white;*/
+                        display: flex;
+                        flex-direction: column;
+                        font-size: 1.5rem;
                         color: white;
+                        gap: 0.7rem;
                     }}
 
                     label {{
@@ -229,13 +244,14 @@ def webPage():
                     
                     .datos p {{
                         margin: 0 0 0 1rem;
+                        white-space: nowrap;
                     }}
                     
                     @media (max-width: 768px) {{
                         
                         .contenedor-variables {{
-                            display: flex;
-                            flex-direction: column;
+                            font-size: 1.25rem;
+                            width: 100%;
                         }}
                     }}
                     
@@ -248,10 +264,10 @@ def webPage():
                     <div class="contenedor">
                         <h2>Datos</h2>
                         <div class="contenedor-variables">
-                            <div class="datos"><label for="temp">Temperatura ambiente</label><meter id="temp" value ="{temp}" min="0" max="50"></meter><p>{temp + " °C"}</p></div>
+                            <div class="datos mover"><label for="temp">Temperatura ambiente</label><meter id="temp" value ="{temp}" min="0" max="50"></meter><p>{temp + " °C"}</p></div>
                             <div class="datos"><label for="hum">Humedad ambiente</label><meter id="hum" value="{hum}" min="0" max="100"></meter><p>{hum + " %"}</p></div>
-                            <div class="datos"><label for="hum_tierra">Humedad de la tierra</label><meter optimum="1000" high="3000" min="1" max="4095" value= "{humedadSuelo}" id="hum_tierra"></meter></div>
-                            <div class="datos"><label for="lum">Luminosidad:</label><p>{ldrEstado}</p></div>
+                            <div class="datos"><label for="hum_tierra">Humedad de la tierra</label><meter low="" optimum="1000" high="3000" min="1" max="4095" value= "{soilHumidity}" id="hum_tierra"></meter></div>
+                            <div class="datos"><label for="lum">Luminosidad:</label><p>{ldrState}</p></div>
                             <div class="datos"><label for="riego">Última vez regado:</label><p></p></div>
                         </div>
                         <div id= "boton">
@@ -262,12 +278,16 @@ def webPage():
             </body>
             </html>
             """
-    return html
+    return html 
+    
     
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(('', 80))
 s.listen(5)
+
+tim0 =Timer(0)                                                              # Este timer se activa cada 1 hora (3600000 milisegundos)  // 10000 ms TEST        
+tim0.init(period=10000, mode=Timer.PERIODIC, callback=getStates)            # para verificar el estado    
 
 while True:
         print("Hilo 0")  
