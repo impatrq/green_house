6dbf2a9c2c2ad66da0468ee2dccd5e261fd46afe
JuanSaz <juan.snchez03@gmail.com>
2022-09-20 10:43:19 -0300

    wip: comenzar a implementar el js para los modals

--- página web/js/app.js
+++ página web/js/app.js
@@ -17,47 +17,53 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 
-//Funcionalidad Sign Up
-// document.getElementById("signup").addEventListener("click", function () {
-//     const email = document.getElementById("email").value;
-//     const password = document.getElementById("password").value;
-//     const span = document.getElementById("error-message");
-//     createUserWithEmailAndPassword(auth, email, password)
-//         .then((userCredential) => {
-//             const user = userCredential.user;
-//             console.log("User Created")
-//             open("../index.html", "_self");                      
-//         })
-//         .catch((error) => {
-//             const errorCode = error.code;
-//             console.log (errorCode)
-//             span.style.visibility = 'visible';
-//             span.textContent = errorCode;
-//         });
-// })
+//Sign Up logic
+document.getElementById("signup").addEventListener("signUp", function () {   //Click event triggers this function from Sign Up Button
+    const email = document.getElementById("email").value;                   //Stores email data
+    const password = document.getElementById("password").value;             //Stores password data
+    const span = document.getElementById("error-message");                  //Gets span reference to show errors
+    createUserWithEmailAndPassword(auth, email, password)                   //Creates user from gathered data
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log("User Created")
+            open("../index.html", "_self");                      
+        })
+        .catch((error) => {                                                 //If data isn't OK, it won't creat user, e.g not using @ or not valid password
+            const errorCode = error.code;
+            console.log (errorCode)
+            span.style.visibility = 'visible';
+            span.textContent = errorCode;
+        });
+})
 
-//Funcionalidad Login
-// document.getElementById("login").addEventListener("click", function () {
-//     const email = document.getElementById("email").value;
-//     const password = document.getElementById("password").value;
-//     const span = document.getElementById("error-message");
-//     signInWithEmailAndPassword(auth, email, password)
-//         .then((userCredential) => {
-//             console.log("User Signed In")
-//             const user = userCredential.user;
-//             open("../index.html", "_self");                      
-//         })
-//         .catch((error) => {
-//             const errorCode = error.code;
-//             console.log (errorCode)
-//             span.style.visibility = 'visible';
-//             span.textContent = errorCode;
-//         });
-// })
+//Login logic
+document.getElementById("login").addEventListener("signIn", function () { //Click event triggers this function from Sign In Button 
+    const email = document.getElementById("email").value;                //Stores email data
+    const password = document.getElementById("password").value;          //Stores password data
+    const span = document.getElementById("error-message");               //Gets span reference to show errors
+    signInWithEmailAndPassword(auth, email, password)                    //This functions validates data from database. If user exists, it'll sign in
+        .then((userCredential) => {
+            console.log("User Signed In")
+            const user = userCredential.user;
+            open("../index.html", "_self");                      
+        })
+        .catch((error) => {                                              //If it doesn't exist:
+            const errorCode = error.code;
+            console.log (errorCode)
+            span.style.visibility = 'visible';
+            span.textContent = errorCode;
+        });
+})
 
-const modal = document.getElementById("modalSignUp");
-
-
-document.getElementById("botonAuth").addEventListener("click", function (){
+//Forma Parte Button
+document.getElementById("botonAuth").addEventListener("formaParte", function (){
     console.log("Forma Parte was clicked");
+    //Show Sign Up Modal
+})
+
+const signUpDiv = document.getElementById("signUp_div");  //Gets signUpDiv reference
+const signInDiv = document.getElementById("singIn_div");  //Gets singInDiv reference
+
+document.getElementById("cerrar").addEventListener("close", function (){
+    //Close signUp and signIn
 })
