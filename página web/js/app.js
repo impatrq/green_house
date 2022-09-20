f76136db8e9fa411c7b93308457963263708ab2c
JuanSaz <juan.snchez03@gmail.com>
2022-09-20 11:39:49 -0300

    wip: contiruar con la integracion del js
    Se arreglaron algunos fallos en cuanto a nulabilidad. Se agregaron lineas de codigo comentadas para no influir con la logica.

--- página web/js/app.js
+++ página web/js/app.js
@@ -17,53 +17,54 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 
-//Sign Up logic
-document.getElementById("signup").addEventListener("signUp", function () {   //Click event triggers this function from Sign Up Button
-    const email = document.getElementById("email").value;                   //Stores email data
-    const password = document.getElementById("password").value;             //Stores password data
-    const span = document.getElementById("error-message");                  //Gets span reference to show errors
-    createUserWithEmailAndPassword(auth, email, password)                   //Creates user from gathered data
-        .then((userCredential) => {
-            const user = userCredential.user;
-            console.log("User Created")
-            open("../index.html", "_self");                      
-        })
-        .catch((error) => {                                                 //If data isn't OK, it won't creat user, e.g not using @ or not valid password
-            const errorCode = error.code;
-            console.log (errorCode)
-            span.style.visibility = 'visible';
-            span.textContent = errorCode;
-        });
-})
+// //Sign Up logic
+// document.getElementById("signup").addEventListener("signUp", function () {  //Click event triggers this function from Sign Up Button
+//     const email = document.getElementById("email").value;                   //Stores email data
+//     const password = document.getElementById("password").value;             //Stores password data
+//     const span = document.getElementById("error-message");                  //Gets span reference to show errors
+//     createUserWithEmailAndPassword(auth, email, password)                   //Creates user from gathered data
+//         .then((userCredential) => {
+//             const user = userCredential.user;
+//             console.log("User Created")
+//             open("../index.html", "_self");                      
+//         })
+//         .catch((error) => {                                                 //If data isn't OK, it won't creat user, e.g not using @ or not valid password
+//             const errorCode = error.code;
+//             console.log (errorCode)
+//             span.style.visibility = 'visible';
+//             span.textContent = errorCode;
+//         });
+// })
 
-//Login logic
-document.getElementById("login").addEventListener("signIn", function () { //Click event triggers this function from Sign In Button 
-    const email = document.getElementById("email").value;                //Stores email data
-    const password = document.getElementById("password").value;          //Stores password data
-    const span = document.getElementById("error-message");               //Gets span reference to show errors
-    signInWithEmailAndPassword(auth, email, password)                    //This functions validates data from database. If user exists, it'll sign in
-        .then((userCredential) => {
-            console.log("User Signed In")
-            const user = userCredential.user;
-            open("../index.html", "_self");                      
-        })
-        .catch((error) => {                                              //If it doesn't exist:
-            const errorCode = error.code;
-            console.log (errorCode)
-            span.style.visibility = 'visible';
-            span.textContent = errorCode;
-        });
-})
+// //Login logic
+// document.getElementById("login").addEventListener("signIn", function () { //Click event triggers this function from Sign In Button 
+//     const email = document.getElementById("email").value;                //Stores email data
+//     const password = document.getElementById("password").value;          //Stores password data
+//     const span = document.getElementById("error-message");               //Gets span reference to show errors
+//     signInWithEmailAndPassword(auth, email, password)                    //This functions validates data from database. If user exists, it'll sign in
+//         .then((userCredential) => {
+//             console.log("User Signed In")
+//             const user = userCredential.user;
+//             open("../index.html", "_self");                      
+//         })
+//         .catch((error) => {                                              //If it doesn't exist:
+//             const errorCode = error.code;
+//             console.log (errorCode)
+//             span.style.visibility = 'visible';
+//             span.textContent = errorCode;
+//         });
+// })
 
 //Forma Parte Button
-document.getElementById("botonAuth").addEventListener("formaParte", function (){
+document.getElementById("botonAuth").addEventListener("click", function (){
     console.log("Forma Parte was clicked");
-    //Show Sign Up Modal
+    //Show Sign Up or Sign In Modal
+    //modal.style.visibility = 'visible';
 })
 
-const signUpDiv = document.getElementById("signUp_div");  //Gets signUpDiv reference
-const signInDiv = document.getElementById("singIn_div");  //Gets singInDiv reference
+// const signUpDiv = document.getElementById("signUp_div");  //Gets signUpDiv reference
+// const signInDiv = document.getElementById("singIn_div");  //Gets singInDiv reference
 
-document.getElementById("cerrar").addEventListener("close", function (){
+//document.getElementById("cerrar").addEventListener("click", function (){
     //Close signUp and signIn
-})
+//})
