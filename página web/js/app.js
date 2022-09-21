9ce67cae6055dd741cef652b6f1dc922afcaefce
JuanSaz <juan.snchez03@gmail.com>
2022-09-21 15:48:07 -0300

    feat: finalizar de agregar js y funcionalidad completa en la pagina
    La pagina es funcional, hay cosas para arreglar pero son detalles menores.

--- página web/js/app.js
+++ página web/js/app.js
@@ -17,56 +17,82 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 
-const signUpDiv = document.getElementById("signUp_div");  //Gets signUpDiv reference
-const signInDiv = document.getElementById("singIn_div");  //Gets singInDiv reference
+var signUpDiv = document.getElementById("signUpDiv");  //Gets signUpDiv reference
+var signInDiv = document.getElementById("signInDiv");  //Gets singInDiv reference
+var backModal = document.getElementById("back_modal");  //Gets Black Background when modal is being showed
+var sideMenu = document.getElementById("offcanvasNavbar");  //Gets sideMenu reference
 
 //Sign Up logic
-document.getElementById("signup").addEventListener("click", function () {  //Click event triggers this function from Sign Up Button
+document.getElementById("signup").addEventListener("click", function () {   //Click event triggers this function from Sign Up Button
     const email = document.getElementById("email").value;                   //Stores email data
     const password = document.getElementById("password").value;             //Stores password data
-    const span = document.getElementById("error-message");                  //Gets span reference to show errors
+    //const span = document.getElementById("error-message");                //Gets span reference to show errors
     createUserWithEmailAndPassword(auth, email, password)                   //Creates user from gathered data
         .then((userCredential) => {
             const user = userCredential.user;
-            console.log("User Created")
-            open("../index.html", "_self");                      
+            console.log("User Created");
+            const IP = prompt("Ingrese la IP del Display: ");
+            console.log(IP);
+            window.open(IP, '_blank');
         })
         .catch((error) => {                                                 //If data isn't OK, it won't creat user, e.g not using @ or not valid password
             const errorCode = error.code;
-            console.log (errorCode)
-            span.style.visibility = 'visible';
-            span.textContent = errorCode;
+            alert(errorCode);
         });
 })
 
-// //Login logic
-// document.getElementById("login").addEventListener("signIn", function () { //Click event triggers this function from Sign In Button 
-//     const email = document.getElementById("email").value;                //Stores email data
-//     const password = document.getElementById("password").value;          //Stores password data
-//     const span = document.getElementById("error-message");               //Gets span reference to show errors
-//     signInWithEmailAndPassword(auth, email, password)                    //This functions validates data from database. If user exists, it'll sign in
-//         .then((userCredential) => {
-//             console.log("User Signed In")
-//             const user = userCredential.user;
-//             open("../index.html", "_self");                      
-//         })
-//         .catch((error) => {                                              //If it doesn't exist:
-//             const errorCode = error.code;
-//             console.log (errorCode)
-//             span.style.visibility = 'visible';
-//             span.textContent = errorCode;
-//         });
-// })
-
-//Forma Parte Button
-document.getElementById("botonAuth").addEventListener("click", function (){
-    signUpDiv.classList.remove("hid");
-    //Show Sign Up or Sign In Modal
-
+//Login logic
+document.getElementById("signin").addEventListener("click", function () { //Click event triggers this function from Sign In Button 
+    const email = document.getElementById("email-login").value;                //Stores email data
+    const password = document.getElementById("password-login").value;          //Stores password data
+    //const span = document.getElementById("error-message");               //Gets span reference to show errors
+    signInWithEmailAndPassword(auth, email, password)                    //This functions validates data from database. If user exists, it'll sign in
+        .then((userCredential) => {
+            console.log("User Signed In")
+            const user = userCredential.user;
+            const IP = prompt("Ingrese la IP del Display: ");
+            console.log(IP);
+            window.open(IP, '_blank');
+        })
+        .catch((error) => {                                              //If it doesn't exist:
+            const errorCode = error.code;
+            console.log(errorCode);
+        });
 })
 
-document.getElementById("boton-cerrar").addEventListener("click", function (){
-    signUpDiv.classList.add("hid");
+//Forma Parte Button
+document.getElementById("boton-Auth").addEventListener("click", function (){
+    signUpDiv.classList.remove("hid");
+    backModal.classList.remove("hid");
+})
+
+//Forma Parte Button Mobile (Side Menu)
+document.getElementById("boton-Auth-mobile").addEventListener("click", function (){
+    signUpDiv.classList.remove("hid");
+    backModal.classList.remove("hid");
+    sideMenu.classList.remove("show");
+})
+
+//Close signIn
+document.getElementById("boton-cerrar-signin").addEventListener("click", function (){
     signInDiv.classList.add("hid");
-    //Close signUp and signIn
+    backModal.classList.add("hid");
+})
+
+//Close signUp
+document.getElementById("boton-cerrar-signup").addEventListener("click", function (){
+    signUpDiv.classList.add("hid");
+    backModal.classList.add("hid");
+})
+
+//Anchor inside signUp Modal
+document.getElementById("iniciar-Sesion-Anchor").addEventListener('click',function(){
+    signUpDiv.classList.add("hid");
+    signInDiv.classList.remove("hid");
+})
+
+//Anchor inside signIn modal
+document.getElementById("crear-Sesion-Anchor").addEventListener('click',function(){
+    signInDiv.classList.add("hid");
+    signUpDiv.classList.remove("hid");
 })
