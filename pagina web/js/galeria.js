9b6b7c0d4dbdab940dd4802d28517c5eb7cb771c
Mateo <unbenitez@gmail.com>
2022-10-19 10:26:18 -0300

    fix: arreglar mal funcionamiento de script galeria

--- pagina web/js/galeria.js
+++ pagina web/js/galeria.js
@@ -15,7 +15,6 @@ document.addEventListener("DOMContentLoaded", ()=> {
                 img.classList.add("hid");
             }
         });
-
         counter++;
         if(counter > imgs.length - 1) {
             counter = 0;
@@ -23,3 +22,10 @@ document.addEventListener("DOMContentLoaded", ()=> {
 
     }, 3000);
 });
+
+let header = document.querySelector("header")
+let body = document.querySelector("body")
+let mainGaleria = document.querySelector(".galeria_main")
+let resultado = body.clientHeight - header.clientHeight
+mainGaleria.style.height = resultado + "px"
+console.log(mainGaleria.height)
